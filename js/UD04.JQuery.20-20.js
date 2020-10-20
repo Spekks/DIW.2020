21e71fefc101a195f6397f94d4adce5dd36ade4d
Spekks <hectoroc88@gmail.com>
2020-10-20 11:22:15 +0200

    20/10 martes. UD04.JQuery.20-10.html/js added

---
+++ js/UD04.JQuery.20-20.js
@@ -0,0 +1,68 @@
+// -- Ejercicio 1 -- 
+/*
+$(document).ready(function() {
+    $("#ocultar").click(function() {
+        $("*").hide();
+    });
+});
+
+// -- Ejercicio 2 -- 
+$(document).ready(function() {
+    $("#ocultar").click(function() {
+        $("#ocultar").hide();
+    });
+});
+
+// -- Ejercicio 3 -- 
+$(document).ready(function() {
+    $('#ocultar').click(function() {
+        $('p.intro').hide();
+    });
+});
+
+// -- Ejercicio 4 --
+$(document).ready(function() {
+    $('#ocultar').click(function() {
+        $('p:last').hide();
+    });
+});
+
+// -- Ejercicio 5 --
+$(document).ready(function() {
+    $('#ocultar').click(function() {
+        $('div:first > ul li:eq(2)').hide();
+    });
+});
+
+// -- Ejercicio 6 --
+$(document).ready(function() {
+    $('#ocultar').click(function() {
+        $('ul li:eq(2)').hide();
+    });
+});
+
+// -- Ejercicio 7 --
+$(document).ready(function() {
+    $('#ocultar').click(function() {
+        $('a').hide();
+    });
+});
+
+
+// -- Ejercicio 8 --
+$(document).ready(function() {
+    $('#ocultar').click(function() {  // Botón ocultar
+        $('a[target!=_blank]').hide();
+    });
+    $('#ocultar-blank').click(function() {  // botón ocultar A:blank
+        $('a[target~=_blank]').hide();
+    });
+});
+*/
+
+// -- Ejercicio 9 --
+$(document).ready(function() {
+    $('').click(function() {
+        $('').hide();
+    });
+});
